7fc41ac6e72c1d9565b3ac7e4770856647ec1924
heron <hpliegocrespo@safareyes.es>
2022-12-16 13:12:17 +0100

    Primer Comentario

--- src/main/java/sge/proyectoerp/ERPController.java
+++ src/main/java/sge/proyectoerp/ERPController.java
@@ -3,7 +3,7 @@ package sge.proyectoerp;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class ERPController {
     @FXML
     private Label welcomeText;
 
