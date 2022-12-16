6826bdff43a72cbd6a1c48fb9eebb9ececd97433
heron <hpliegocrespo@safareyes.es>
2022-12-16 14:24:45 +0100

    Empezandfo Fxml

--- src/main/java/sge/proyectoerp/ERPApplication.java
+++ src/main/java/sge/proyectoerp/ERPApplication.java
@@ -7,11 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class ERPApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("erp.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(ERPApplication.class.getResource("erp.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setMaximized(true);
         stage.setTitle("Macaquisho");
         stage.setScene(scene);
         stage.show();
--- src/main/java/sge/proyectoerp/ERPController.java
+++ src/main/java/sge/proyectoerp/ERPController.java
@@ -1,14 +1,25 @@
 package sge.proyectoerp;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class ERPController {
     @FXML
-    private Label welcomeText;
+    private Pane Menu;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+    /*Menu.getStylesheets().clear();
+        Menu.getStylesheets().add("src/main/resources/sge/proyectoerp/css/darkmode.css");*/
+
+    @FXML
+    void initialize() {
+
     }
 }
