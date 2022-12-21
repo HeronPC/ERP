712a547f05bbd28a73f1f149ceeba6e52c8b66d7
Manuel <torrecito98@gmail.com>
2022-12-21 09:51:36 +0100

    Arreglado Macaquishp

--- src/main/java/sge/proyectoerp/ERPApplication.java
+++ src/main/java/sge/proyectoerp/ERPApplication.java
@@ -12,7 +12,7 @@ public class ERPApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ERPApplication.class.getResource("erp.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setMaximized(true);
+        //stage.setMaximized(true);
         stage.setTitle("Macaquisho");
         stage.setScene(scene);
         stage.show();
