5839313e53722e13ceb5b9ec9e87555704af7384
Manuel Torres <mtorrespulido@safareyes.es>
2023-01-17 12:10:24 +0100

    BDDUsers añadido

--- src/main/java/sge/proyectoerp/ERPApplication.java
+++ src/main/java/sge/proyectoerp/ERPApplication.java
@@ -4,9 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-import sge.proyectoerp.Controllers.ERPController;
 
 import java.io.IOException;
 import java.util.Objects;
